a1d6bf7641a0f129a42d04549653ff1cabedc5c8
shivsaxena2005 <shivsaxena2878@gmail.com>
2025-09-25 09:52:54 +0530

    with requirements.txt

--- disease/disease/main.py
+++
@@ -1,8 +0,0 @@
-# caller.py
-from test import run_pipeline   # replace with the filename where run_pipeline is defined
-
-# Example 1: Get output in English
-result_english = run_pipeline("brown rust", language=1)
-print("\n=== English Output ===")
-print(result_english)
-
